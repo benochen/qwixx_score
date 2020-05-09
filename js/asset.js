2be9a9bba6f8b637a61642466548dc0082dfd09e
benoit <benoit.chenal.57@gmail.com>
2020-05-10 00:28:37 +0200

    load resource

---
+++ js/asset.js
@@ -0,0 +1,47 @@
+var assetURL=[
+    "/asset/img/dice/b_1.png",
+    "/asset/img/dice/b_2.png",
+    "/asset/img/dice/b_3.png",
+    "/asset/img/dice/b_4.png",
+    "/asset/img/dice/b_5.png",
+    "/asset/img/dice/b_6.png",
+    "/asset/img/dice/r_1.png",
+    "/asset/img/dice/r_2.png",
+    "/asset/img/dice/r_3.png",
+    "/asset/img/dice/r_4.png",
+    "/asset/img/dice/r_5.png",
+    "/asset/img/dice/r_6.png",
+    "/asset/img/dice/y_1.png",
+    "/asset/img/dice/y_2.png",
+    "/asset/img/dice/y_3.png",
+    "/asset/img/dice/y_4.png",
+    "/asset/img/dice/y_5.png",
+    "/asset/img/dice/y_6.png",
+    "/asset/img/dice/g_1.png",
+    "/asset/img/dice/g_2.png",
+    "/asset/img/dice/g_3.png",
+    "/asset/img/dice/g_4.png",
+    "/asset/img/dice/g_5.png",
+    "/asset/img/dice/g_6.png",
+    "/asset/img/dice/w1_1.png",
+    "/asset/img/dice/w1_2.png",
+    "/asset/img/dice/w1_3.png",
+    "/asset/img/dice/w1_4.png",
+    "/asset/img/dice/w1_5.png",
+    "/asset/img/dice/w1_6.png",
+    "/asset/img/dice/w2_1.png",
+    "/asset/img/dice/w2_2.png",
+    "/asset/img/dice/w2_3.png",
+    "/asset/img/dice/w2_4.png",
+    "/asset/img/dice/w2_5.png",
+    "/asset/img/dice/w2_6.png"
+
+
+]
+
+
+for(var x=0;x<assetURL.length;x++)
+{
+   let image = new Image();
+   image.src=assetURL[x];
+}
